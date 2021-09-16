44fa4d7d403b58f218bab73a95dddee61a0f7abc
ahmedmustahid <ahmed@epx.phys.tohoku.ac.jp>
2021-09-16 20:51:04 +0900

    skeleton Japanese cv

---
+++ src/metadata/metadata.js
@@ -0,0 +1,73 @@
+module.exports = {
+  name: 'ムスタヒド　アハメド',
+  title: '',
+  facts: {
+    '住所': '練馬区、東京都',
+    '国籍': 'バングラデシュ',
+    'Git Profile': `<a href="https://github.com/ahmedmustahid">
+                  <span class="print">https://github.com/ahmedmustahid</span>
+                  <span class="screen">Github</span>
+                </a>`,
+    'Email': '<a href="mailto:amustahid25@gmail.com">amustahid25@gmail.com</a>'
+  },
+  skills: [
+    ['Python', '++++'],
+    ['C++17', '++++'],
+    ['C', '++++'],
+    ['Dart/Flutter', '++++'],
+    ['Swift', '+++'],
+    ['Azure', '+++'],
+    ['AWS', '++++'],
+    ['統計学', '++++'],
+    ['画像解析', '++++'],
+    ['自然言語処理', '+++'],
+    ['アルゴリズム', '++++'],
+    ['データ構造', '++++'],
+  ],
+  projects: [
+    {
+      title: 'AI チャットボットアプリ',
+      written: '6月 2021 - 現在',
+      badges: ['AWS', 'Dart/flutter', 'Swift','Docker', 'Git'],
+      contents: `
+        To be filled later
+      `
+    },
+    {
+      title: 'データベースデザイン・構築',
+      written: '2月 2021 – 5月 2021',
+      badges: ['MySQL', 'Azure', 'Python', 'Git'],
+      contents: `
+        Later
+      `
+    },
+{
+      title: '物流最適化',
+      written: '12月 2020 - 2月 2021',
+      badges: ['Python', 'C++', 'Git'],
+
+      contents: `
+          Later
+        `
+    },
+    {
+      title: '画像に対する異常認識',
+      written: '5月 2020 - 11月 2020 ',
+      badges: ['Python', 'MySQL', 'Azure', 'Docker', 'Git'],
+      contents: `
+        Later
+      `
+    }
+  ],
+  additional_experience: [
+    {
+      header: '修士研究：機械学習・画像認識・データ解析',
+      skills: ['Python', 'C++', 'Git'],
+      contents: `
+        Later
+      `
+    },
+
+  ],
+  
+};
